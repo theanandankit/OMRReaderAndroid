35d8962063862e69cbcd4bc2061e306169a9359b
theanandankit <ankitanand2909@gmail.com>
2020-11-14 02:01:37 +0530

    initial commit

---
+++ app/src/main/java/com/Project/OMRReader/Activity/loginActivity.java
@@ -0,0 +1,16 @@
+package com.Project.OMRReader.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Project.OMRReader.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
