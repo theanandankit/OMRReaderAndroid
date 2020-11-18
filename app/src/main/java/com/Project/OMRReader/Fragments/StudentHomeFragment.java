e36ac1454689aa0233df41b43cc2c41364b82700
anmol14dev <img_2018012@iiitm.ac.in>
2020-11-19 03:41:47 +0530

    Student last Quiz Added

--- app/src/main/java/com/Project/OMRReader/Fragments/StudentHomeFragment.java
+++ app/src/main/java/com/Project/OMRReader/Fragments/StudentHomeFragment.java
@@ -1,5 +1,6 @@
 package com.Project.OMRReader.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.Project.OMRReader.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 public class StudentHomeFragment extends Fragment {
+    PieChart pieChart;
+    int[] color = new int[]{Color.GREEN,Color.RED};
 
 
 
@@ -19,6 +28,31 @@ public class StudentHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_student_home, container, false);
+        pieChart = view.findViewById(R.id.pieChart);
+        setPieChart();
+
+
         return view;
+    }
+    private void setPieChart(){
+        PieDataSet pieDataSet = new PieDataSet(pieData(),"");
+        pieDataSet.setColors(color);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setUsePercentValues(false);
+        pieChart.setDrawEntryLabels(false);
+        pieChart.getDescription().setEnabled(false);
+
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+    }
+
+    private ArrayList<PieEntry> pieData() {
+        ArrayList<PieEntry> list = new ArrayList<>();
+        list.add(new PieEntry(60,"Correct"));
+        list.add(new PieEntry(40,"wrong"));
+        return list;
     }
 }
